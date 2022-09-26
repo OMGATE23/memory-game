98b41336e2ddef3fc584a3e10dc5450aa84508ee
omgate <90276220+OMGATE23@users.noreply.github.com>
2022-09-26 22:43:48 +0530

    feat: added flipped and disable properties

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import './Card.css'
+
+export default function Card({card, handleChoice, flipped, disable}) {
+
+    function clickHandler(){
+        if(!disable){
+            handleChoice(card)
+        }
+    }
+  return (
+    <div className='card'>
+        <div className={flipped ? 'flipped' : ""}>
+            <img src= {card.src} alt='front' className='front'/>
+            <img 
+                src= './project-images/cover.png' 
+                alt='back'
+                onClick={clickHandler}
+                className ="back"
+            />
+        </div>
+    </div>
+  )
+}
